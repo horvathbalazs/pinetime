baf76ec0e6dd3f1a55aae789bb60a1cf4dafab8c
Horváth Balázs <balazs.horvath@email.com>
2021-08-05 22:44:06 +0200

    Update partials

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,7 @@
+import { Sine, TweenMax } from 'gsap/all'
+import MorphSVGPlugin from 'vendor/morphsvg'
+
 import SliderHero from 'partials/slider/hero'
 import Sandwich from 'partials/sandwich'
+
+const plugins = [ MorphSVGPlugin ]
--- resources/js/partials/sandwich.js
+++ resources/js/partials/sandwich.js
@@ -1,5 +1,62 @@
+import { Sine, TweenMax } from 'gsap/all'
+import { watch } from 'watchjs'
+
+import variables from 'mixins/variables'
+
 class Sandwich {
     constructor() {
+        this.references = {}
+        this.state      = ['default']
+        this.variables  = {}
+
+        this.references['sandwich'] = document.querySelector('[data-sandwich]')
+        this.references = Array.from(document.querySelectorAll('[data-sandwich-path]'))
+            .reduce((accumulator, element) => {
+                accumulator[element.getAttribute('data-sandwich-path')] = element
+                return accumulator
+            },
+            this.references)
+
+        this.variables['navigation'] = variables.navigation
+
+        this.references.sandwich.addEventListener('click', () => this.click())
+        this.references.sandwich.addEventListener('mouseover', () => this.hover())
+        this.references.sandwich.addEventListener('mouseleave', () => this.leave())
+
+        watch(this, 'state', () => this.morph())
+    }
+
+    click() {
+        if (this.state.includes('active')) {
+            this.state = this.state.filter(action => action !== 'active')
+        } else {
+            this.state.push('active')
+        }
+    }
+
+    hover() {
+        if (!this.state.includes('hover')) {
+            this.state.push('hover')
+        }
+    }
+
+    leave() {
+        this.state = this.state.filter(action => action !== 'hover')
+    }
+
+    morph() {
+        let morph = 'default'
+
+        if (this.state.includes('active')) {
+            morph = 'active'
+        } else if (this.state.includes('hover')) {
+            morph = 'hover'
+        }
+
+        TweenMax.to(this.references.main, this.variables.navigation, {
+            ease: Sine.easeInOut,
+            morphSVG: { shape: this.references[morph].getAttribute('d') }
+        })
     }
 }
 
